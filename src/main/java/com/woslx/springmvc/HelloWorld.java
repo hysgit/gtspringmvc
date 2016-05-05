3dbe74f6335bd264c13aa7ee9d5e705be64d4a56
huangyue <hy@zizizizizi.com>
2016-05-05 14:59:16 +0800

    init

---
+++ src/main/java/com/woslx/springmvc/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.woslx.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by hy
+ * Date: 16-5-5.
+ */
+@Controller
+public class HelloWorld {
+    @RequestMapping("/helloworld")
+    public String Hello() {
+        System.out.println("HelloWorld!");
+        return "success";
+    }
+}

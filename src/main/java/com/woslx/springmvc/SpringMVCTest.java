f0964fd8034e5a7a9197b031f279d485b125d065
huangyue <hy@zizizizizi.com>
2016-05-07 00:02:01 +0800

    4.1 POST

--- src/main/java/com/woslx/springmvc/SpringMVCTest.java
+++ src/main/java/com/woslx/springmvc/SpringMVCTest.java
@@ -2,6 +2,7 @@ package com.woslx.springmvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by hy on 16-5-6.
@@ -10,6 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/springmvc")
 public class SpringMVCTest {
+
+    @RequestMapping(value="/testMethod",method = RequestMethod.POST)
+    public String testMethod()
+    {
+        System.out.println("testMethod");
+        return "success";
+    }
 
     @RequestMapping("/testRequestMapping")
     public String testRequestMapping()
